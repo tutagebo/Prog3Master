cb34029b1fcd2727bc59c86c2ebf3a95073e4b7b
tutannnn <koudai.nattoufu@gmail.com>
2024-11-04 17:11:21 +0900

    class6

--- Prog3Master/prog0501.cpp
+++ Prog3Master/prog0501.cpp
@@ -1,7 +1,7 @@
 ﻿#include <iostream>
 #include <stdlib.h>
+#include <Windows.h>
 #pragma warning(disable:4996)
-#define N 50
 
 //ノードの構造体
 typedef struct node {
@@ -10,12 +10,13 @@ typedef struct node {
 	node* left;
 	node* right;
 } node;
+
 //木をたどって値を昇順に出力する
-void print_value(FILE* fpw, node* now) {
+void print_value(node* now) {
 	if (now == NULL) return;
-	print_value(fpw, now->left);
+	print_value(now->left);
 	printf("%d\n", now->score);
-	print_value(fpw, now->right);
+	print_value(now->right);
 	return;
 }
 //木をたどってメモリを解放
@@ -33,18 +34,18 @@ void draw_tree(FILE* fpt, node* p1, float x, float y, float width) {
 	int score = p1->score;
 	fprintf(fpt, "%8.5f, %8.5f, %d \n", x, -y, score);
 	width /= 2;
-	draw_tree(fpt, p1->left, x-width, y+1, width);
+	draw_tree(fpt, p1->left, x - width, y + 1, width);
 	fprintf(fpt, "%8.5f, %8.5f, %d \n", x, -y, score);
 	width *= 2;
-	draw_tree(fpt, p1->right, x+width, y+1, width);
+	draw_tree(fpt, p1->right, x + width, y + 1, width);
 	fprintf(fpt, "%8.5f, %8.5f, %d \n", x, -y, score);
 }
 
 //木の高さを計算する関数
 int height_tree(node* now) {
 	if (now == NULL) return 0;
-	int left = height_tree(now->left)+1;
-	int right = height_tree(now->right)+1;
+	int left = height_tree(now->left) + 1;
+	int right = height_tree(now->right) + 1;
 	return (left > right) ? left : right;
 }
 
@@ -52,7 +53,7 @@ int height_tree(node* now) {
 void maketree(node* p1, int id, int score) {
 	node* p2;
 	if (p1->score <= score) {
-		if(p1->right==NULL){
+		if (p1->right == NULL) {
 			p2 = (node*)malloc(sizeof(node));
 			p2->id = id;
 			p2->score = score;
@@ -73,50 +74,67 @@ void maketree(node* p1, int id, int score) {
 			p2->right = NULL;
 			p1->left = p2;
 		}
-		else { 
+		else {
 			maketree(p1->left, id, score);
 		}
 	}
 }
+/*
+int search_node(node* p1, int score) {
+	if (p1 == NULL)return -1;
+	if (score == p1->score)return p1->id;
+	return (score < p1->score) ? search_node(p1->left, score) : search_node(p1->right, score);
+}*/
 
-// main関数
-int main()
-{
-	FILE* fpi; //入力ファイル
-	FILE* fpw; //昇順にソートしたファイル
-	FILE* fpt; //木を描くためのファイル
-	if (fopen_s(&fpi, "data_huge.txt", "r") != 0) {
-		printf("Failed to open file: data_small.txt\n");
-		return -1;
-	}
-	if (fopen_s(&fpw, "order.txt", "w") != 0) {
-		printf("Failed to open file: order.txt\n");
-		return -1;
-	}
-	if (fopen_s(&fpt, "tree.xls", "w") != 0) {
-		printf("Failed to open file: tree.xls\n");
-		return -1;
-	}
-	int data[100000],i=0;
-	while (fscanf_s(fpi, "%d", &data[i++]) != EOF);
-	for (int e : data) {
-		printf("%d\n", e);
-	}
-	fclose(fpi);
-	node* p1; //根(root)ノード
-	p1 = (node*)malloc(sizeof(node));
-	p1->id = 0;
-	p1->score = data[0];
-	p1->left = p1->right = NULL;
-	for (i = 0; i < N; i++) {
-		maketree(p1, i, data[i]);
-	}
-	print_value(fpw, p1);
-	draw_tree(fpt, p1, 0, 0, 1);
-	int height;
-	height = height_tree(p1);
-	printf("height: %d\n", height);
-	finish(p1);
-	return 0;
+int search_node(node* p1, int score) {
+	if (p1 == NULL) return -1;  // ノードが存在しない場合、-1を返す
+	if (p1->score == score) return p1->id;  // スコアが一致すればIDを返す
+	if (score < p1->score)
+		return search_node(p1->left, score);  // 左部分木に移動
+	else
+		return search_node(p1->right, score); // 右部分木に移動
 }
 
+void main() {
+	int target_ary[10] = {
+		30391024, 328291059, 501372422, 637875300, 311223120,
+		603117998, 621736584, 140666598, 31012956, 935691960
+	};
+	FILE* fpi; //入力ファイル
+	if (fopen_s(&fpi, "data_huge.txt", "r") != 0) {
+		printf("Failed to open file: data_huge.txt\n");
+		return;
+	}
+	//二分木作成
+	int data, i = 0;
+	node* p1; //根(root)ノード
+	fscanf_s(fpi, "%d", &data);
+	p1 = (node*)malloc(sizeof(node));
+	p1->id = 0;
+	p1->score = data;
+	p1->left = p1->right = NULL;
+	i++;
+	while (fscanf_s(fpi, "%d", &data) != EOF){
+		maketree(p1, i, data);
+		i++;
+	}
+	print_value(p1);
+	//ここまで
+	for (i = 0; i < 10; i++) {
+		LARGE_INTEGER frequency;
+		QueryPerformanceFrequency(&frequency);
+		LARGE_INTEGER start, end;
+		QueryPerformanceCounter(&start);
+		// koko
+		int res = search_node(p1, target_ary[i]);
+		printf("%d", res);
+		// kokomade
+		QueryPerformanceCounter(&end);
+		LONGLONG span = end.QuadPart - start.QuadPart;
+		double sec = (double)span / (double)frequency.QuadPart;
+		printf("%d:%lf\n", res, sec);
+	}
+	finish(p1);
+	fclose(fpi);
+	return;
+}
---
+++ Prog3Master/prog601.cpp
@@ -0,0 +1,201 @@
+﻿#include <iostream>
+#include <stdlib.h>
+#include <Windows.h>
+#include <math.h>
+#define N 50
+#pragma warning(disable:4996)
+
+// セル(cell)
+typedef struct cell {
+	char* score;
+	int id;
+	struct cell* next;
+} cell;
+// ハッシュテーブル
+typedef struct hashtable {
+	int size, count;
+	cell** table;
+} hashtable;
+
+hashtable* make_hashtable(int size);
+int hash_func(hashtable* ht, const char* score);
+bool insert_hash(hashtable* ht, const char* score, int id);
+cell* make_cell(const char* score, int id, cell* next);
+
+// ハッシュ表の生成・初期化
+hashtable* make_hashtable(int size) {
+	hashtable* ht = (hashtable*)malloc(sizeof(hashtable));
+	if (ht != NULL) {
+		ht->size = size;
+		ht->count = 0;
+		ht->table = (cell**)malloc(sizeof(cell*) * size);
+		if (ht->table == NULL) {
+			free(ht);
+			return NULL;
+		}
+		for (int i = 0; i < size; i++) ht->table[i] = NULL;
+	}
+	return ht;
+}
+// ハッシュ関数
+int hash_func(hashtable* ht, const char* score) {
+	unsigned int value = 0;
+	for (int n = 0; score[n] != '\0'; n++)
+		value = (value << 3) + score[n];
+	return value % ht->size;
+}
+// 挿入
+bool insert_hash(hashtable* ht, const char* score, int id) {
+	int hval = hash_func(ht, score);
+	ht->table[hval] = make_cell(score, id, ht->table[hval]);
+	ht->count++;
+	return true;
+}
+// セルの生成
+cell* make_cell(const char* score, int id, cell* next) {
+	cell* cp = (cell*)malloc(sizeof(cell));
+	if (cp != NULL) {
+		cp->score = (char*)malloc(strlen(score) + 1);
+		strcpy(cp->score, score);
+		cp->id = id;
+		cp->next = next;
+	}
+	return cp;
+}
+
+void print_hashtable(hashtable* ht) {
+	for (int hval = 0; hval < ht->size; hval++) {
+		printf("hval=%d score ", hval);
+		cell* next_cell = ht->table[hval];
+		while (next_cell != NULL) {
+			printf("%s ", next_cell->score);
+			next_cell = next_cell->next;
+		}
+		printf("\n");
+	}
+}
+
+int search_hash(hashtable* ht, const char* score) {
+	int hash = hash_func(ht, score);
+	cell* now_cell = ht->table[hash];
+	while (now_cell!=NULL) {
+		if (strcmp(score, now_cell->score) == 0) {
+			printf("%d\n", now_cell->id);
+			return 0;
+		}
+		now_cell = now_cell->next;
+	}
+	return 0;
+}
+
+// ハッシュ表の廃棄
+void delete_hashtable(hashtable* ht) {
+	for (int i = 0; i < ht->size; i++) {
+		cell* cp = ht->table[i];
+		while (cp != NULL) {
+			cell* xs = cp->next;
+			free(cp->score);
+			free(cp);
+			cp = xs;
+		}
+	}
+	free(ht);
+}
+
+void hash_test0(){
+	hashtable* ht = make_hashtable(100003);
+	char strings[5][30] = { "ikaho","numata","kusatsu","mizusawa","haruna" };
+	for (int i = 0; i < 5;i++) {
+		int hash = hash_func(ht, strings[i]);
+		printf("%-10s: %d\n", strings[i], hash);
+	}
+	delete_hashtable(ht);
+}
+
+void hash_test1(int num_data) {
+	hashtable* ht = make_hashtable(100003);
+	for (int i = 0; i < num_data; i++) {
+		char string[1000];
+		int tar_num = rand() * rand();
+		sprintf_s(string, "%d", tar_num);
+		insert_hash(ht, string, i);
+	}
+	delete_hashtable(ht);
+}
+
+void hash_test2() {
+	FILE* fpi; //入力ファイル
+	if (fopen_s(&fpi, "data_small.txt", "r") != 0) {
+		printf("Failed to open file: data_small.txt\n");
+		return;
+	}
+	int i=0;
+	hashtable* ht = make_hashtable(18);
+	int tmp_num;
+	char tmp[50];
+	while (fscanf_s(fpi, "%d", &tmp_num) != EOF) {
+		sprintf_s(tmp, "%d", tmp_num);
+		insert_hash(ht, tmp, i);
+		i++;
+	}
+	print_hashtable(ht);
+	delete_hashtable(ht);
+}
+
+void prog604a(){
+//void main() {
+	char target_ary[10][12] = {
+		"30391024", "328291059", "501372422",
+		"637875300", "311223120", "603117998", 
+		"621736584", "140666598", "31012956", "935691960"
+	};
+	FILE* fpi; //入力ファイル
+	if (fopen_s(&fpi, "data_huge.txt", "r") != 0) {
+		printf("Failed to open file: data_huge.txt\n");
+		return;
+	}
+	//ハッシュ表作成
+	int i = 0;
+	hashtable* ht = make_hashtable(100003);
+	//int tmp_num;
+	int tmp_num;
+	char tmp[50];
+	while (fscanf_s(fpi, "%d", &tmp_num) != EOF) {
+		sprintf_s(tmp, "%d", tmp_num);
+		insert_hash(ht, tmp, i);
+		i++;
+	}
+	fclose(fpi);
+	//ここまで
+	for (i = 0; i < 10; i++) {
+		LARGE_INTEGER frequency;
+		QueryPerformanceFrequency(&frequency);
+		LARGE_INTEGER start, end;
+		QueryPerformanceCounter(&start);
+		// koko
+		search_hash(ht,target_ary[i]);
+		// kokomade
+		QueryPerformanceCounter(&end);
+		LONGLONG span = end.QuadPart - start.QuadPart;
+		double sec = (double)span / (double)frequency.QuadPart;
+		printf("%d:%lf\n", i, sec);
+	}
+	delete_hashtable(ht);
+}
+
+int prog602a() {
+	for (int i = 10000; i <= 250000; i += 10000) {
+		LARGE_INTEGER frequency;
+		QueryPerformanceFrequency(&frequency);
+		LARGE_INTEGER start, end;
+		QueryPerformanceCounter(&start);
+		// koko
+		hash_test1(i);
+		// kokomade
+		QueryPerformanceCounter(&end);
+		LONGLONG span = end.QuadPart - start.QuadPart;
+		double sec = (double)span / (double)frequency.QuadPart;
+		printf("%d:%lf\n", i, sec);
+	}
+	return 0;
+}
