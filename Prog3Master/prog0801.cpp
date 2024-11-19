dc4c659905eb0f107ea66e6aa715fffc476710a0
tutannnn <koudai.nattoufu@gmail.com>
2024-11-19 16:10:29 +0900

    class9

---
+++ Prog3Master/prog0801.cpp
@@ -0,0 +1,98 @@
+﻿#include <iostream>
+#include <stdlib.h>
+#include <Windows.h>
+#pragma warning(disable:4996)
+#define COUNTOF(array) (sizeof(array) / sizeof(array[0]))
+
+void PrintD(int D[], int D_SIZE) {
+	for (int i = 0; i < D_SIZE; i++) {
+		printf("%2d ", D[i]);
+	}
+	printf("\n");
+}
+int hash_func(int D[], int D_SIZE) {
+	char buff[12];
+	unsigned int value = 1;
+	for (int i = 0; i < D_SIZE; i++) {
+		sprintf(buff, "%d", D[i]);
+		for (int n = 0; buff[n] != '¥0'; n++) {
+			int temp = (buff[n] * (i + 77)) % 6700417 + 1;
+			value = ((value)*temp) % 6700417;
+		}
+	}
+	return value;
+}
+void swap(int* a, int* b) {
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+	return;
+}
+
+// PushHeap Algorithm 5.3
+void PushHeap(int T[], int* size, int x) {
+	*size = *size + 1;
+	T[*size] = x;
+	int k = *size;
+	while (T[k] > T[k / 2]) {
+		swap(&T[k], &T[k / 2]);
+		k /= 2;
+	}
+}
+
+// DeleteMaxinum Algorithm 5.2
+int DeleteMaxinum(int T[], int* size) {
+	int max, k, big;
+	max = T[1];
+	T[1] = T[*size];
+	T[*size] = 0;
+	*size = *size - 1;
+	k = 1;
+	while (2 * k <= *size) {
+		if(!T[k*2]||!T[k*2+1]) {
+			if (T[k] < T[k * 2])swap(&T[k], &T[k * 2]);
+			else break;
+		}
+		else {
+			big = (T[k * 2] > T[k * 2 + 1]) ? k * 2 : k * 2 + 1;
+			if (T[k] < T[big])swap(&T[k], &T[big]);
+			else break;
+		}
+	}
+	return max;
+}
+
+int prog801(void) {
+	FILE* fpi;
+	const int D_SIZE = 100000;
+	int D[100000] = {};
+	if (fopen_s(&fpi, "random_100k.txt", "r") != 0) {
+		printf("Failed to open file\n");
+		return 0;
+	}
+	for (int i = 0; fscanf_s(fpi, "%d", &D[i]) != EOF; i++);
+	for (int x : D) {
+		printf("%d\n", x);
+	}
+	//タイマースタート
+	//配列Tの初期化
+	int* T = (int*)malloc((D_SIZE + 1) * sizeof(int));
+	if (T != NULL) {
+		for (int i = 0; i <= D_SIZE; i++) {
+			T[i] = 0;
+		}
+	}
+	int size = 0;
+	for (int i = 0; i < D_SIZE; i++) {
+		PushHeap(T, &size, D[i]);
+	}
+	for (int i = D_SIZE - 1; i >= 0; i = i - 1) {
+		D[i] = DeleteMaxinum(T, &size);
+	}
+	//タイマー終了 計算時間出力
+	//ソート後の配列に対するハッシュ値
+	printf("hash value = %d", hash_func(D, D_SIZE));
+	free(T);
+	free(D);
+}
---
+++ Prog3Master/prog0901.cpp
@@ -0,0 +1,95 @@
+﻿#include <iostream>
+#include <stdlib.h>
+#include <Windows.h>
+#pragma warning(disable:4996)
+#define COUNTOF(array) (sizeof(array) / sizeof(array[0]))
+
+typedef struct record {
+	char city[50];
+	char country[50];
+	double data[20];
+}record;
+
+void PrintD(int D[], int D_SIZE) {
+	for (int i = 0; i < D_SIZE; i++) {
+		printf("%2d ", D[i]);
+	}
+	printf("\n");
+}
+int hash_func(int D[], int D_SIZE) {
+	char buff[12];
+	unsigned int value = 1;
+	for (int i = 0; i < D_SIZE; i++) {
+		sprintf(buff, "%d", D[i]);
+		for (int n = 0; buff[n] != '\0'; n++) {
+			int temp = (buff[n] * (i + 77)) % 6700417 + 1;
+			value = ((value)*temp) % 6700417;
+		}
+	}
+	return value;
+}
+void swap(record* a, record* b) {
+	record temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+	return;
+}
+
+
+int Partition(record D[], int left, int right, int target) {
+	int k = (left + right) / 2;
+	swap(&D[k], &D[right]);
+	int i = left;
+	int j = right - 1;
+	while (i <= j) {
+		while (D[i].data[target] < D[right].data[target])i++;
+		while (D[j].data[target] >= D[right].data[target] && j >= i)j--;
+		if (i < j)swap(&D[i], &D[j]);
+	}
+	swap(&D[i], &D[right]);
+	return i;
+}
+
+int QuickSort(record D[], int left, int right, int target) {
+	if (left < right) {
+		int pivot_index = Partition(D, left, right, target);
+		QuickSort(D, left, pivot_index - 1, target);
+		QuickSort(D, pivot_index + 1, right, target);
+	}
+	return 0;
+}
+
+int main(void) {
+	FILE* fpi;
+	fopen_s(&fpi, "city_population.txt", "r");
+	if (fpi == NULL) {
+		printf("読み込みエラーです。¥n");
+		return -1;
+	}
+	int D_SIZE = 1692;
+	int target = 9;
+	record* D = (record*)(malloc(sizeof(record) * D_SIZE));
+	if (D == NULL) {
+		exit(1);
+	}
+	for (int i = 0; fscanf_s(fpi, "%s", &D[i].city, (unsigned int)sizeof(D[i].city)) != EOF; i++) {
+		fscanf_s(fpi, "%s", &D[i].country, (unsigned int)sizeof(D[i].country));
+		fscanf_s(fpi, "%lf", &D[i].data[0]);
+		fscanf_s(fpi, "%lf", &D[i].data[1]);
+		fscanf_s(fpi, "%lf", &D[i].data[2]);
+		fscanf_s(fpi, "%lf", &D[i].data[3]);
+		fscanf_s(fpi, "%lf", &D[i].data[4]);
+		fscanf_s(fpi, "%lf", &D[i].data[5]);
+		fscanf_s(fpi, "%lf", &D[i].data[6]);
+		fscanf_s(fpi, "%lf", &D[i].data[7]);
+		fscanf_s(fpi, "%lf", &D[i].data[8]);
+		D[i].data[9] = D[i].data[7] / D[i].data[5];
+	}
+	QuickSort(D, 0, D_SIZE - 1, target);
+	for (int i = 0; i < D_SIZE; i++) {
+		printf("%s %s: %lf\n", D[i].city, D[i].country, D[i].data[target]);
+	}
+	free(D);
+	return 0;
+}
