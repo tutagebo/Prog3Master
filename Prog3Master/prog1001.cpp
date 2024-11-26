82f1e35062c425c642dbbb544ca9db2c115977af
tutannnn <koudai.nattoufu@gmail.com>
2024-11-26 16:38:26 +0900

    prog10

---
+++ Prog3Master/prog1001.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <math.h>
+#include <Windows.h>
+#include <math.h>
+#pragma warning(disable:4996)
+#define COUNTOF(array) (sizeof(array) / sizeof(array[0]))
+
+long long Recursive_Sum2(int D[], int left, int right) {
+	int mid = (left + right) / 2;
+	if (left == right)return D[left];
+	long long leftSum = Recursive_Sum2(D, left, mid);
+	long long rightSum = Recursive_Sum2(D, mid + 1, right);
+	return leftSum + rightSum;
+}
+
+long long Simple_Sum(int D[], int n) {
+	long long sum = 0;
+	for (int i = 0; i < n; i++) {
+		sum += D[i];
+	}
+	return sum;
+}
+
+int prog1001(void) {
+	const int D_SIZE = 10 * 1000;
+	int D[D_SIZE] = {};
+	FILE* fpi;
+	if (fopen_s(&fpi, "random_10k.txt", "r") != 0) {
+		printf("Failed to open file\n");
+		return 0;
+	}
+	for (int i = 0; i < D_SIZE && fscanf_s(fpi, "%d", &D[i]) != EOF; i++);
+	fclose(fpi);
+	long long sum;
+	LARGE_INTEGER frequency;
+	QueryPerformanceFrequency(&frequency);
+	LARGE_INTEGER start, end;
+	QueryPerformanceCounter(&start);
+	// koko
+	sum = Simple_Sum(D, D_SIZE);
+	// kokomade
+	QueryPerformanceCounter(&end);
+	LONGLONG span = end.QuadPart - start.QuadPart;
+	double sec = (double)span / (double)frequency.QuadPart;
+	printf("%lf", sec);
+
+	return 0;
+}
---
+++ Prog3Master/prog1002.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <stdlib.h>
+#include <Windows.h>
+#include <math.h>
+#pragma warning(disable:4996)
+#define COUNTOF(array) (sizeof(array) / sizeof(array[0]))
+
+long long Bproduct(int X[], int Y[], int n) {
+	int sum = 0, power = 1;
+	for (int i = 0; i < n; i++) {
+		int s = 0, p = power;
+		for (int j = 0; j < n; j++) {
+			s += p * X[i] * Y[j];
+			p *= 10;
+		}
+		sum += s;
+		power *= 10;
+	}
+	return sum;
+}
+
+long long Product(int X[], int Y[], int n) {
+	if (n == 1) return X[0] * Y[0];
+	int h = n / 2;
+	int* X1 = (int*)malloc(sizeof(int) * h); int* Y1 = (int*)malloc(sizeof(int) * h);
+	int* X2 = (int*)malloc(sizeof(int) * h); int* Y2 = (int*)malloc(sizeof(int) * h);
+	int* X12 = (int*)malloc(sizeof(int) * h); int* Y12 = (int*)malloc(sizeof(int) * h);
+	for (int i = 0; i < h; i++) {
+		X12[i] = X[i] + X[h + i]; Y12[i] = Y[i] + Y[h + i];
+		X2[i] = X[h + i]; Y2[i] = Y[h + i];
+		X1[i] = X[i]; Y1[i] = Y[i];
+	}
+	long long c = Product(X1, Y1, h);
+	long long b = Product(X12, Y12, h);
+	long long a = Product(X2, Y2, h);
+	free(X12); free(Y12); free(X2);
+	free(Y2); free(X1); free(Y1);
+	return a * pow(10, n) + (b - (a + c)) * pow(10, n / 2) + c;
+}
+
+
+int main(int argc, const char* argv[]) {
+	int X[] = { 7 };
+	int Y[] = { 9 };
+	int size = COUNTOF(X);
+	if ((size & (size - 1))) {
+		printf("Size is not a power of 2. ¥n");
+		exit(0);
+	}
+	int repeat = 10000000;
+	long long prod;
+	LARGE_INTEGER frequency;
+	QueryPerformanceFrequency(&frequency);
+	LARGE_INTEGER start, end;
+	QueryPerformanceCounter(&start);
+	for (int i = 0; i < repeat; i++) {
+		prod = Bproduct(X, Y, size);
+	}
+	QueryPerformanceCounter(&end);
+	LONGLONG span = end.QuadPart - start.QuadPart;
+	double sec = (double)span / (double)frequency.QuadPart;
+	printf("sec = %lf", sec);
+	return 0;
+}
